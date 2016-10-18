e10cb004dca138e5f5e7e034561efaded03e0724
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-10-18 17:16:24 +0200

    Update 2016-10-18T15:15:39.972Z

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import npm from "rollup-plugin-node-resolve";
-
-export default {
-  entry: "rollup-entry.js",
-  format: "umd",
-  moduleName: "d3",
-  plugins: [npm({jsnext: true})],
-  dest: "_app/scripts/vendor/d3-bundle.js"
-};
